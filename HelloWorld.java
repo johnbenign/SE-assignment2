58957bb6176eeb28799e4362f9f685f212999e06
Benign Ihugba <bihugba@pvamu.edu>
2025-10-16 02:24:11 -0500

    Added a new feature to print out my name: Benign John

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         // Print the message to the console
         System.out.println("Hello, World!");
+
+        // Print a personalized greeting
+        System.out.println("Hi! Benign John");
     }
 }
